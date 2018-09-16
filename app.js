c8586d3d6b5f0ec80d8da4ab208baf9df14f7bb0
muhammet turan <muhammed.turan@outlook.com>
2018-09-16 03:23:20 +0300

    json web token verify

--- app.js
+++ app.js
@@ -18,6 +18,9 @@ const db  = require('./helper/db')();
 const config  = require('./config');
 app.set('api_secret_key', config.api_secret_key);
 
+//middleware
+const verifyToken = require('./middleware/verify-token');
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -31,6 +34,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', index);
+app.use('/api', verifyToken);
 app.use('/api/movies', movie);
 app.use('/api/directors', director);
 
---
+++ middleware/verify-token.js
@@ -0,0 +1,26 @@
+const jwt =  require('jsonwebtoken');
+
+
+module.exports  = (req, res,  next) => {
+  const token =  req.header['x-access-token'] || req.body.token || req.query.token;
+
+  if (token){
+    jwt.verify(token, req.app.get('api_secret_key'), (err, decoded) =>{
+      if (err){
+        res.json({
+          status: false,
+          message: 'failed to authenticate token.'
+        });
+      }else{
+        req.decode  = decoded;
+        next();
+      }
+    });
+  }else{
+    res.json(
+      {
+        status: false,
+        message: 'No token provided.'
+      });
+  }
+}
